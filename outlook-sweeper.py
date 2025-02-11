96ac5d06f105c5e9ae55df8f303264366948b0a3
Ben Briles <brbriles@iu.edu>
2025-02-11 17:37:51 -0500

    targets specific outlook folder and cleans up WIP files at end of script

--- outlook-sweeper.py
+++ outlook-sweeper.py
@@ -5,51 +5,53 @@ import os
 import re
 
 outlook = Dispatch("Outlook.Application").GetNamespace("MAPI")
-inbox = outlook.GetDefaultFolder(6)
-print(inbox)
-messages = inbox.items
-message = messages.GetLast()
-name = str(message.subject)
-#to eliminate any special charecters in the name
-name = re.sub('[^A-Za-z0-9]+', '', name)+'.msg'
-#to save in the current working directory
-message.SaveAs(os.getcwd()+'//'+name)
+folder = outlook.Folders.Item(2)
+sweeper_folder = folder.Folders['Sweeper Testing'] # will need to be updated to account for the specific production environment; probably set this up to take user input for folder selection
+messages = sweeper_folder.items
 
-f = name
-msg = extract_msg.Message(f)
-msg_sender = msg.sender
-msg_date = msg.date
-msg_subj = msg.subject
-msg_message = msg.body
-msg.close()
+for message in messages:
+    name = str(message.subject)
+    #to eliminate any special charecters in the name
+    name = re.sub('[^A-Za-z0-9]+', '', name)+'.msg'
+    #to save in the current working directory
+    message.SaveAs(os.getcwd()+'//'+name)
 
-print('Sender: {}'.format(msg_sender))
-print('Sent On: {}'.format(msg_date))
-print('Subject: {}'.format(msg_subj))
-print('Body: {}'.format(msg_message))
+    f = name
+    msg = extract_msg.Message(f)
+    msg_sender = msg.sender
+    msg_date = msg.date
+    msg_subj = msg.subject
+    msg_message = msg.body
+    msg.close()
 
-html = "<html><head></head><body><p>" + msg.body.replace('\n', "<br>") + "</p></body></html>"
+    print('Sender: {}'.format(msg_sender))
+    print('Sent On: {}'.format(msg_date))
+    print('Subject: {}'.format(msg_subj))
+    print('Body: {}'.format(msg_message))
 
-text_file = open("Output.html", "w")
-text_file.write(html)
-text_file.close()
+    html = "<html><head></head><body><p>" + msg.body.replace('\n', "<br>") + "</p></body></html>"
 
-def convert_html_to_pdf(source_html, output_filename):
-    # open output file for writing (truncated binary)
-    result_file = open(output_filename, "w+b")
+    text_file = open("Output.html", "w")
+    text_file.write(html)
+    text_file.close()
 
-    # convert HTML to PDF
-    pisa_status = pisa.CreatePDF(
-            source_html,                # the HTML to convert
-            dest=result_file)           # file handle to recieve result
+    def convert_html_to_pdf(source_html, output_filename):
+        # open output file for writing (truncated binary)
+        result_file = open(output_filename, "w+b")
 
-    # close output file
-    result_file.close()                 # close output file
+        # convert HTML to PDF
+        pisa_status = pisa.CreatePDF(
+                source_html,                # the HTML to convert
+                dest=result_file)           # file handle to recieve result
 
-    # return False on success and True on errors
-    return pisa_status.err
+        # close output file
+        result_file.close()                 # close output file
 
-# Define your data
-source_html = open('Output.html')
-output_filename = f'{name}.pdf'
-convert_html_to_pdf(source_html, output_filename)
+        # return False on success and True on errors
+        return pisa_status.err
+
+    # Define your data
+    source_html = open('Output.html')
+    output_filename = f'{name}.pdf'
+    convert_html_to_pdf(source_html, output_filename)
+    os.remove(name)
